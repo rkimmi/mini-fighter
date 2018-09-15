758d7ac8b84e9deb592cbf70ba20cbf7f1725511
rkimmi <kimmirindel97@gmail.com>
2018-09-16 08:37:13 +1200

    Set up files, add player states

---
+++ script.js
@@ -0,0 +1,14 @@
+let playerOne = {
+    fullHealth: 1000, lastDamage: 0, 
+    moves: {
+        '65': -5, '87': -10, '68': -15, '83': -7 // A, W, D, S
+    }
+}
+
+let playerTwo = {
+    fullHealth: 1000, lastDamage: 0,
+    moves: {
+        '37': -5, '38': -10, '39': -15, '40': -7 // L, UP, R, DO
+    }
+}
+
